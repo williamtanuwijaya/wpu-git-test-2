4db410c654db23f0e539d686fbcf3fe8e5561d65
williamtanuwijaya <will.tw0807@gmail.com>
2022-08-20 16:31:57 +0700

    mengubah file index.php

--- index.php
+++ index.php
@@ -11,7 +11,9 @@
 <body>
 	<h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi minima esse impedit dicta veritatis quidem amet repudiandae omnis praesentium dolorum, voluptatibus ipsam quod accusantium laboriosam ipsum assumenda atque doloribus cupiditate!</p>
-
+    <ul>
+        <li>Uccok</li>
+    </ul>
 	<script type="text/javascript" src="script.js"></script>
 </body>
 </html>

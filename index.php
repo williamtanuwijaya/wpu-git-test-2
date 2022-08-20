2a7da7b8fdb93ed5253188343fb4a395529fdc9f
williamtanuwijaya <will.tw0807@gmail.com>
2022-08-20 16:24:29 +0700

    menambahkan file index.html

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Coba Git Remote</title>
+	<style>
+		body {
+	font-family: arial;
+}
+	</style>
+</head>
+<body>
+	<h1>Hello World!</h1>
+
+	<script type="text/javascript" src="script.js"></script>
+</body>
+</html>
